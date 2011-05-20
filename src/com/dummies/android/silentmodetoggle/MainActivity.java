226377b19db4f7206fecfa0fb94fdd5ab6730970
lycar <lmaicasm@terra.es>
2011-05-20 15:13:05 +0200

    first place

---
+++ src/com/dummies/android/silentmodetoggle/MainActivity.java
@@ -0,0 +1,90 @@
+package com.dummies.android.silentmodetoggle;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+	private AudioManager mAudioManager;
+	private boolean mPhoneIsSilent;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        
+        checkIfPhoneIsSilent();
+        
+        setButtonClickListener();
+        
+        Log.d("SilentModeApp", "This is a test");
+        
+    }
+    
+	private void setButtonClickListener(){
+    
+    	Button toggleButton = (Button)findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+    		
+    		public void onClick(View v) {
+    			
+    			if (mPhoneIsSilent){
+    				//Change back to normal mode
+    				mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+    				mPhoneIsSilent=false;
+    			} else {
+    				//Change to silent mode
+    				mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+    				mPhoneIsSilent=true;
+    			}
+    			//Now toggle the UI again
+    			toggleUI();
+    		}
+    	});
+    }
+    
+	/** 
+	 * Checks to see if the phone is currently in silent mode
+	 */
+    private void checkIfPhoneIsSilent() {
+    	
+    	int ringerMode = mAudioManager.getRingerMode();
+    	
+    	if (ringerMode == AudioManager.RINGER_MODE_SILENT){
+    		mPhoneIsSilent=true;
+    	} else {
+    		mPhoneIsSilent=false;
+    	}
+	}
+    /**
+     * Toggles the UI images from silent to normal and vice versa.
+     */
+    private void toggleUI() {
+    	
+    	ImageView imageView = (ImageView) findViewById(R.id.phone_icon);
+    	Drawable newPhoneImage;
+    	
+    	if (mPhoneIsSilent) {
+    		newPhoneImage = getResources().getDrawable(R.drawable.phoneoff);
+    	} else {
+    		newPhoneImage = getResources().getDrawable(R.drawable.phoneon);
+    	}
+    	
+    	imageView.setImageDrawable(newPhoneImage);
+    }
+    
+    protected void onResume(){
+    	super.onResume();
+    	checkIfPhoneIsSilent();
+    	toggleUI();
+    }
+}
